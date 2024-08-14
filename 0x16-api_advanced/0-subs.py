f93bdbde78cd2f1ab2cd7292b17d73afbb715723
mrxdoffical <berogergesg@gmail.com>
2024-08-14 13:18:36 +0300

    fixed things

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,10 +14,10 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
-    
+
     try:
         results = response.json().get("data", {})
         return results.get("subscribers", 0)
